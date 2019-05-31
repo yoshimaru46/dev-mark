a75333b6abe04e3fb1619bf51c5cb8c5a8783dfd
yshr446 <yshr446@gmail.com>
2019-06-01 02:00:33 +0900

    Small fix

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   chrome.storage.local.get(null, (configs) => {
-    if (message.method == 'getConfig') {
+    if (message.method === 'getConfig') {
       sendResponse({ data: configs.devMarkConfig });
     }
   });
